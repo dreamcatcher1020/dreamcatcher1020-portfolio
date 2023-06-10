2a8f5b7776996c45f97d5d0aa3d4a7437db22f6d
dreamcatcher1020 <dream.catcherd21@gmail.com>
2023-06-10 10:08:20 -0500

    experience, project

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,16 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
+        company: 'Sparkybit',
         jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        startYear: '2021',
+        endYear: '2022'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
-    },
-    {
-        id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'Dataforest',
+        jobtitle: 'Fullstack Developer',
+        startYear: '2022',
+        endYear: '2023'
     },
 ]
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,75 +11,30 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Modern eCommerce Course',
+        projectDesc: 'Learn the Shopify + Next.js + Tailwind CSS Stack! SWR, Hydrogen, + more',
+        tags: ['Shopify', 'Next.js', 'TailwindCSS', 'SWR', 'Hydrogen'],
+        code: 'https://github.com/dreamcatcher1020/Shopify-Next.js-Tailwind',
+        demo: 'https://www.buildnextshop.com/',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Crypto Boy NFT',
+        projectDesc: 'An NFT marketplace where users can mint ERC721 implemented Crypto Boy NFTs and manage them on the Ethereum Blockchain.',
+        tags: ['Node', 'Ethereum', 'Dapp', 'Smart Contracts', 'Solidity', 'NFT', 'NFT-marketplace'],
+        code: 'https://github.com/dreamcatcher1020/NFT-Marketplace-Cryptoboys',
+        demo: 'https://devpavan04.github.io/cryptoboys-nft-marketplace/',
         image: two
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Future of furniture',
+        projectDesc: "It's a ecommerce website with react and tailwind css",
+        tags: ['React', 'eCommerce', 'TailwindCSS'],
+        code: 'https://github.com/dreamcatcher1020/eCommerce-Furniture-Website',
+        demo: 'https://ecommerce-furniture-website.netlify.app/',
         image: three
-    },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
     },
 ]
 
