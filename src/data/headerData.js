98c885a7979d07bec806793d2249a9ddd68eaf6f
dreamcatcher1020 <dream.catcherd21@gmail.com>
2023-06-09 21:02:26 -0500

    change avatar

---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import avatar from '../assets/png/avatar.png'
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Vladyslav Baranov',
+    title: "Web Developer",
+    desciption:"The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.",
+    image: avatar,
+    resumePdf: resume
+}
