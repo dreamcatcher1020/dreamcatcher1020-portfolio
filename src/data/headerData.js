9f1fac23a1aabc6175644f278e79edd6f12f0d14
dreamcatcher1020 <dream.catcherd21@gmail.com>
2023-06-12 23:41:08 -0500

    resume, avatar

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import avatar from '../assets/png/avatar.png'
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Daniel Lee',
+    name: 'Martin Lee',
     title: "Web Developer",
     desciption:"The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.",
     image: avatar,
